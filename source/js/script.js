ba24db7eb4f2af63797c671ddc0802a8f9c1408d
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2024-05-07 13:16:25 +0300

    Calculator

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,7 @@ class App {
   init() {
     this.onButtonPlay();
     this.isPolicyChecked();
-
+    this.initRange();
   }
 
   constructor() {
@@ -23,7 +23,7 @@ class App {
     this.btnPlay.addEventListener('click', playVideo);
   }
 
-    isPolicyChecked() {
+  isPolicyChecked() {
     const leadform1 = document.querySelector('#leadform1');
     const leadform2 = document.querySelector('#leadform2');
 
@@ -47,6 +47,75 @@ class App {
     }
 
     [leadform1, leadform2].forEach((f) => { f.addEventListener('click', isChecked(f)) })
+  }
+
+  initRange() {
+    const butns = document.querySelectorAll('.profit-btn')
+    const calcValue = document.querySelector('#calcResult')
+    const profitValue = document.querySelector('#profitValue')
+    let month = 6
+
+    const getValueFromText = (text) => {
+      return parseInt(text.replace(/[\s€]/g, ""));
+    }
+
+    const setMonth = (amount) => {
+      month = +amount
+    }
+
+    const resetBtnsStyles = () => {
+      butns.forEach((b) => { b.classList.remove('profit-btn--active') })
+    }
+
+    const getMulti = (month) => {
+      if (month === 0) {
+        return 1.52;
+      }
+
+      if (month === 1) {
+        return 1.92;
+      }
+
+      if (month === 6) {
+        return 2.32;
+      }
+
+      if (month === 12) {
+        return 2.72;
+      }
+    };
+
+    butns.forEach((b) => {
+      b.addEventListener('click', () => {
+        resetBtnsStyles()
+        b.classList.add('profit-btn--active')
+        setMonth(b.id);
+        profitValue.innerText = (Math.round(getValueFromText(calcValue.innerText) * getMulti(month)) + ' €')
+      });
+    });
+
+    $(function () {
+      $(".js-range-slider").ionRangeSlider({
+        skin: "round",
+        hide_min_max: false,
+        hide_from_to: true,
+        min: 250,
+        max: 10000,
+        from: 1400,
+        postfix: " €",
+        grid: false,
+        prettify: function (value) {
+          return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+      },
+        onStart: function (data) {
+          $("#calcResult").text(data.from.toString() + ' €');
+        },
+        onChange: function (data) {
+          $("#profitValue").text(Math.round(data.from * getMulti(month)) + ' €');
+          $("#calcResult").text(data.from.toString() + ' €');
+        },
+      });
+    });
   }
 }
 
